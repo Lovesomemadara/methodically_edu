1b52f9a5f0b5cd1a410cad4ddbbf24c35c3de64d
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-09-18 10:06:50 +0100

    Проверка ДЗ: Работа с файлами

--- working_with_files_9/task_2/2.py
+++ working_with_files_9/task_2/2.py
@@ -1,6 +1,4 @@
 with open('city.txt', mode='r', encoding='UTF-8') as reader:
-    line_count: int = 0
-    for line in reader.readlines():
-        line_count += 1
+    line_count: int = len(reader.readlines())
 
 print(f'Количество строк в файле: {line_count}')
--- working_with_files_9/task_5/5.py
+++ working_with_files_9/task_5/5.py
@@ -4,11 +4,24 @@ with (open('surnames.txt', mode='r', encoding='UTF-8') as reader,
     content: list[str] = reader.readlines()
 
     fem_chars: list[str] = ['ВА', 'НА', 'АЯ']
+    women: list[str] = []
+    men: list[str] = []
+
     for name in content:
         last_letters: str = name.rstrip()[-2:]
-        add_name: str = name.rstrip().capitalize() + '\n'
+        add_name: str = name.rstrip().capitalize()
 
         if last_letters in fem_chars:
-            writer_wom.write(add_name)
+            women.append(add_name)
         else:
-            writer_man.write(add_name)
+            men.append(add_name)
+
+    # [
+    #     women.append(name.rstrip().capitalize())
+    #     if name.rstrip()[-2:] in fem_chars
+    #     else men.append(name.rstrip().capitalize())
+    #     for name in content
+    # ]
+
+    writer_wom.write('\n'.join(women))
+    writer_man.write('\n'.join(men))
